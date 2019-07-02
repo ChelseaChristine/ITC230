af3c740d1692450144c00a2e4814aa90e4947f07
Chelsea J <43831285+ChelseaChristine@users.noreply.github.com>
2019-07-01 18:14:06 -0700

    initial upload of HW1
    adds all files related to HW1 for ITC230

--- index.js
+++ index.js
@@ -6,22 +6,19 @@ const callback = (req, res) =>{
     switch(path) {
       case '/':
         const fs = require("fs");
-        console.log("Step 1")
-        fs.readFile("home.html", (err, data) => {
-        console.log("Step 2")
+        fs.readFile("public/home.html", (err, data) => {
         if (err) return console.error(err);
             res.writeHead(200, {'Content-Type': 'text/html'});
             res.end(data.toString());
         });
-        console.log("Step 3")
         break;
       case '/about':
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end('About page');
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.end('this application is for utilizing in my ITC230 class');
         break;
       default:
         res.writeHead(404, {'Content-Type': 'text/plain'});
-        res.end('Not found');
+        res.end('Oops! 404, Page Not found');
         break;
       }
     }
