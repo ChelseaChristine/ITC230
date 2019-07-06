e497d51c3b970e9289eef373b758891cc872d198
Chelsea J <43831285+ChelseaChristine@users.noreply.github.com>
2019-07-05 18:01:25 -0700

    adds updated index with reference to modules
    Also with updated module method names

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const http = require("http");
+let animal = require('./lib/module');
 
 const callback = (req, res) =>{
-    const path = req.url.toLowerCase();
-
+  const querystring = require('querystring');
+  let url = req.url.split("?");
+  let path = url[0].toLowerCase();
+  let queryParams = querystring.parse(url[1]);
+  
     switch(path) {
       case '/':
         const fs = require("fs");
@@ -15,6 +19,18 @@ const callback = (req, res) =>{
       case '/about':
         res.writeHead(404, {'Content-Type': 'text/plain'});
         res.end('this application is for utilizing in my ITC230 class');
+        break;
+      case '/detail':
+        res.writeHead(200, {'Content-Type' : 'text/plain'});
+        let found = animal.getItem(queryParams.name);
+        let result = (found) ? JSON.stringify(found) : "Not found";
+        res.end('Searching for ' +queryParams.name +"\n" + result);
+        break;
+      case '/delete':
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        let remove = animal.deleteItem(queryParams.name);
+        let confirm = (remove) ? queryParams.name + " Not Removed" : queryParams.name+ " Removed";
+        res.end(confirm);
         break;
       default:
         res.writeHead(404, {'Content-Type': 'text/plain'});
