cf608a6aff876f38ac05c13e09577161a52109c1
Chelsea J <43831285+ChelseaChristine@users.noreply.github.com>
2019-07-09 19:08:45 -0700

    changes index.js to express
    Also adds views and a layout got .post for details that shows the results

--- index.js
+++ index.js
@@ -1,42 +1,58 @@
-const http = require("http");
-let animal = require('./lib/module');
+'use strict'
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+let animal = require('./lib/data');
 
-const callback = (req, res) =>{
-  const querystring = require('querystring');
-  let url = req.url.split("?");
-  let path = url[0].toLowerCase();
-  let queryParams = querystring.parse(url[1]);
-  
-    switch(path) {
-      case '/':
-        const fs = require("fs");
-        fs.readFile("public/home.html", (err, data) => {
-        if (err) return console.error(err);
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.end(data.toString());
-        });
-        break;
-      case '/about':
-        res.writeHead(404, {'Content-Type': 'text/plain'});
-        res.end('this application is for utilizing in my ITC230 class');
-        break;
-      case '/detail':
-        res.writeHead(200, {'Content-Type' : 'text/plain'});
-        let found = animal.getItem(queryParams.name);
-        let result = (found) ? JSON.stringify(found) : "Not found";
-        res.end('Searching for ' +queryParams.name +"\n" + result);
-        break;
-      case '/delete':
-        res.writeHead(404, {'Content-Type': 'text/plain'});
-        let remove = animal.deleteItem(queryParams.name);
-        let confirm = (remove) ? queryParams.name + " Not Removed" : queryParams.name+ " Removed";
-        res.end(confirm);
-        break;
-      default:
-        res.writeHead(404, {'Content-Type': 'text/plain'});
-        res.end('Oops! 404, Page Not found');
-        break;
-      }
-    }
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(__dirname + '/public')); // set location for static files
+app.use(bodyParser.urlencoded({extended: true})); // parse form submissions
 
-http.createServer(callback).listen(process.env.PORT || 3000);
+let handlebars =  require("express-handlebars");
+app.engine(".html", handlebars({extname: '.html', defaultLayout: false}));
+app.set("view engine", ".html");
+
+// send static file as response
+app.get('/', (req, res) => {
+  res.type('text/html');
+  res.sendFile(__dirname + '/public/home.html'); 
+ });
+
+ // send plain text response
+app.get('/about', (req, res) => {
+  res.type('text/plain');
+  res.send('this application is for utilizing in my ITC230 class');
+ });
+//gets an animal name query, then either returns with not found or the results
+ app.get('/detail', (req, res) => {
+  res.type('text/plain');
+  let found = animal.getItem(req.query.name);
+  console.log(req.query);
+  let result = (found) ? JSON.stringify(found) : JSON.stringify(req.query.name) + "Not found";
+  res.send('Searching for ' + JSON.stringify(req.query.name) + "\n" + result);
+});
+app.post('/detail', (req, res) => {
+  console.log(req.body); // display parsed form submission
+  let found2 = animal.getItem(req.body.name);
+  res.render('details', {name: req.body.name, result: found2 });
+});
+
+//gets an animal name query, and will remove it if it exists
+app.get('/delete', (req, res) => {
+  res.type('text/plain');
+  let remove = animal.deleteItem(req.query.name);
+  let confirm = (remove) ? req.query.name + " Not Removed" : req.query.name + " Removed";
+  res.send(confirm);
+ });
+
+ // define 404 handler
+app.use( (req,res) => {
+  res.type('text/plain'); 
+  res.status(404);
+  res.send('404 - Not found');
+ });
+
+ app.listen(app.get('port'), () => {
+  console.log('Express started'); 
+ });
+ 
