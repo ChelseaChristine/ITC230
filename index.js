16650ec05c5115bbda3e006c8258f71095e46a0b
Chelsea J <43831285+ChelseaChristine@users.noreply.github.com>
2019-07-25 11:56:51 -0700

    adds methods that work with the mongoDB
    Also updates the index.js paths to be utilized by the new methods.  Still a bug with delete though.

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-let animal = require('./lib/data');
+let Animal = require("./models/animal");
+let animal = require("./lib/animalMethods");
 
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public')); // set location for static files
@@ -13,8 +14,12 @@ app.engine(".html", handlebars({extname: '.html', defaultLayout: false}));
 app.set("view engine", ".html");
 
 // dyanmic response with the home page showing all the animals in the list
-app.get('/', (req, res) => {
-  res.render('home', {animals: animal.getAll()});
+app.get('/', (req, res, next) => {
+  animal.getAll().then((allAnimals) => {
+    res.render('home', {animals: allAnimals }); 
+  }).catch((err) =>{
+    return next(err);
+  });
  });
 
  // send plain text response
@@ -24,24 +29,30 @@ app.get('/about', (req, res) => {
  });
 
 //gets an animal name query, then either returns with not found or the results
- app.get('/detail', (req, res) => {
+ app.get('/detail', (req, res, next) => {
   res.type('text/plain');
-  let found = animal.getItem(req.query.name);
-  console.log(req.query);
-  let result = (found) ? JSON.stringify(found) : JSON.stringify(req.query.name) + "Not found";
-  res.send('Searching for ' + JSON.stringify(req.query.name) + "\n" + result);
+  animal.getItem(req.query.name).then((found) =>{
+    res.render('details',{name: req.query.name, result: found});
+  }).catch((err) =>{
+    return next(err);
+  });
 });
 //runs utilizing a submission form and opens a specific details page
-app.post('/detail', (req, res) => {
-  console.log(req.body); // display parsed form submission
-  let found2 = animal.getItem(req.body.name);
-  res.render('details', {name: req.body.name, result: found2 });
+app.post('/detail', (req, res, next) => {
+  animal.getItem(req.body.name).then((found2) =>{
+    res.render('details',{name: req.body.name, result: found2});
+  }).catch((err) =>{
+    return next(err);
+  });
 });
 
 //gets an animal name query, and will remove it if it exists
-app.get('/delete', (req, res) => {
-  let remove = animal.deleteItem(req.query.name);
-  res.render('delete', {name: req.query.name, result: remove });
+app.get('/delete', (req, res, next) => {
+  animal.deleteItem(req.query.name).then((remove) =>{
+    res.render('delete',{name: req.query.name, result: remove});
+  }).catch((err) =>{
+    return next(err);
+  });
  });
 
  // define 404 handler
